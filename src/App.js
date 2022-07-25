310ab08529333c98089740878b8ea23a390f35a4
arjun rawat <arjunrawat0722@gmail.com>
2022-07-25 19:09:45 +0530

    hey

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+ import BarChart from "./BarChart"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='heading'>Manufac Analytics Private Limited</h1>
+      <h2 className='titleone'>BarChart</h2>
+  <BarChart></BarChart>
+  <h3>e “Alcohol” category on horizontal axis and average of “Malic Acid” on vertical axis.</h3>
+
+    </div>
+  );
+}
+
+export default App;
